e74b98f53898066fa769dff0e99cd87f3ccf2efc
sbv <boris.spitsin@gmail.ru>
2019-04-13 15:24:22 +0600

    Add Photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_125443) do
+ActiveRecord::Schema.define(version: 2019_04_12_105136) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
